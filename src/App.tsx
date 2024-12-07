7fc0634a759d4cef2a0e03fd539a51a4411723cd
yui <yuineor@gmail.com>
2024-12-07 23:34:07 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Router from "./components/Router";
+
+function App() {
+  return (
+    <>
+      {/* <p className="text-6xl text-rose-600">Приветик</p> */}
+      <Router />
+    </>
+  );
+}
+
+export default App;
