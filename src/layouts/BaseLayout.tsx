f0e5be1cff20a4457d6f4744143676b78b9b9fb0
Anerisse <aleksandraneri165@gmail.com>
2024-12-11 14:49:36 +0700

    штука сверху и плюсик

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,8 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import Button from "../components/ui/Button";
+import { ChevronLeft } from "lucide-react"; 
+import { EllipsisVertical } from "lucide-react";
 
-export const BaseLayout = () => {
+
+export const BaseLayout = ({ title = "Главная" }) => {
+  const navigate = useNavigate(); // Hook для навигации
   return (
     <>
+      <div className="sticky top-0 flex flex-row flex-auto items-center relative bg-custom-button-bg">
+        <Button onClick={() => navigate(-1)} className="flex flex-row">
+          <ChevronLeft />
+        </Button>
+        <p className="text-2xl m-auto">{title}</p>
+        <Button>
+          <EllipsisVertical />
+        </Button>
+      </div>
       <div className="p-3">
         <Outlet />
       </div>
--- src/pages/allTasks.tsx
+++ src/pages/allTasks.tsx
@@ -2,18 +2,23 @@ import { useNavigate } from "react-router-dom";
 import { routePaths } from "../lib/path";
 import TasksList from "../components/forms/allTasks/tasksList";
 import Button from "../components/ui/Button";
+import { Plus } from "lucide-react";
 
 export const AllTasks = () => {
   const navigate = useNavigate();
 
   return (
     <div>
-      <Button onClick={() => navigate(routePaths.verification)}>+</Button>
-
-      <p className="text-2xl text-red-600">Задачи</p>
-      <TasksList />
-
-      <Button onClick={() => navigate(-1)}>Назад</Button>
+    
+      
+    <div className="mt-4">
+        <TasksList />
+        <Button onClick={() => navigate(routePaths.verification)}
+        className="fixed bottom-4 right-4 !p-4 rounded-full shadow-lg"><Plus /></Button>
+        
+    </div>
     </div>
   );
+
 };
+
